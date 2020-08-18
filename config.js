8d4a5d74ce636f110ba9279974a65bc577a71ead
Mark Sailes <mark@sailes.co.uk>
2020-08-18 21:28:53 +0100

    config fix

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ module.exports = {
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: 'Open Source',
+      path: '/pages/open-source'
     }
   ],
   author: {
